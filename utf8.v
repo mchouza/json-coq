80c39ae4174ebeb3ab7368518e18611e042fe55c
Mariano M. Chouza <mchouza@gmail.com>
2015-09-18 00:10:58 -0300

    Starting UTF-8 decoder.

---
+++ utf8.v
@@ -0,0 +1,109 @@
+Require Import Ascii.
+Require Import List.
+Require Import String.
+Require Import ZArith.
+
+(** Unicode codepoint literal support, based on
+    https://github.com/arthuraa/poleiro/blob/master/theories/ForceOption.v *)
+
+Open Scope char_scope.
+Open Scope Z_scope.
+
+Fixpoint _parse_hex_digit c :=
+  match c with
+  | "0" => Some 0
+  | "1" => Some 1
+  | "2" => Some 2
+  | "3" => Some 3
+  | "4" => Some 4
+  | "5" => Some 5
+  | "6" => Some 6
+  | "7" => Some 7
+  | "8" => Some 8
+  | "9" => Some 9
+  | "A" | "a" => Some 10
+  | "B" | "b" => Some 11
+  | "C" | "c" => Some 12
+  | "D" | "d" => Some 13
+  | "E" | "e" => Some 14
+  | "F" | "f" => Some 15
+  | _ => None
+  end.
+
+Open Scope string_scope.
+
+Fixpoint _parse_hex_opt_aux s accum :=
+  match s with
+  | "" => Some accum
+  | String c s' => 
+      match _parse_hex_digit c with
+      | Some n => _parse_hex_opt_aux s' (16 * accum + n)
+      | None => None
+      end
+  end.
+
+Definition _parse_hex_opt s := _parse_hex_opt_aux s 0.
+
+Lemma _parse_hex_opt_1: _parse_hex_opt "FFfE" = Some 65534.
+Proof.
+  auto.
+Qed.
+
+Lemma _parse_hex_opt_2: _parse_hex_opt "AAfg" = None.
+Proof.
+  auto.
+Qed.
+
+Inductive hexParseError := HexParseError.
+
+Definition force_some {A} {ErrType} (o:option A) (e:ErrType) :
+  match o with
+  | Some _ => A
+  | None => ErrType
+  end
+:=
+  match o with
+  | Some a => a
+  | None => e
+  end.
+
+Definition parse_hex (s:string) := force_some (_parse_hex_opt s) HexParseError.
+
+Lemma parse_hex_1: (parse_hex "FFfE" - 65534 = 0).
+Proof.
+  auto.
+Qed.
+
+Lemma parse_hex_2: parse_hex "AAfg" = HexParseError.
+Proof.
+  auto.
+Qed.
+
+Notation "U+ s" := (parse_hex s) (at level 0, no associativity).
+
+Lemma U_1: U+"FfFe" = 65534.
+Proof.
+  auto.
+Qed.
+
+Lemma U_2: U+"AAfg" = HexParseError.
+Proof.
+  auto.
+Qed.
+
+Lemma U_3: U+"FfFe" + U+"0002" = 65536.
+Proof.
+  auto.
+Qed.
+
+Lemma U_4: Z.land U+"AA" U+"F0" = U+"A0".
+Proof.
+  auto.
+Qed.
+
+(** UTF-8 description based on https://tools.ietf.org/html/rfc3629#section-3 **)
+
+(** UTF-8 decoding support **)
+
+Definition utf8_char_decode c n :=
+  Z.land (Z.of_N (N_of_ascii c)) ((Z.shiftl 1 n) - 1).
